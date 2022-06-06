04e7007653a876134fca01605e779bf4993536e8
std <std@GREEN-STUDENT9>
2022-06-06 20:10:23 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication41/CompareCirclesTeacher.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "circle.h"
+using namespace std;
+
+int main()
+{
+    Circle c1(5);
+    Circle c2(2);
+    Circle c3(2);
+
+    c3 += 3;
+    std::cout << (c1 == c2);
+    cout << c1;
+}
+
